c2aab954b389125a1d439c8cc48d9da796ff1aa4
Felipe B <99092931+fbreseghello@users.noreply.github.com>
2023-03-11 18:59:16 -0300

    Update display.php
    Acrescentado alerta ao excluir usuário.

--- display.php
+++ display.php
@@ -1,40 +1,44 @@
 <?php
-include 'config.php'; ?>
-
+include 'config.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="edit.css">
-    <title>Administração</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="edit.css">
+  <title>Administração</title>
+  <script>
+    function confirmDelete() {
+      return confirm("Tem certeza que deseja deletar o usuário?");
+    }
+  </script>
 </head>
 
 <body>
-    <div class="container" style="overflow-x:auto;">
-        <table class="box">
-            <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Sobrenome</th>
-                    <th scope="col">Cep</th>
-                    <th scope="col">Rua</th>
-                    <th scope="col">Bairro</th>
-                    <th scope="col">Cidade</th>
-                    <th scope="col">Estado</th>
-                    <th scope="col">Número</th>
-                    <th scope="col">Complemento</th>
-                    <th scope="col">Ação
-                    </th>
-
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+  <div class="container" style="overflow-x:auto;">
+    <table class="box">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Sobrenome</th>
+          <th scope="col">Cep</th>
+          <th scope="col">Rua</th>
+          <th scope="col">Bairro</th>
+          <th scope="col">Cidade</th>
+          <th scope="col">Estado</th>
+          <th scope="col">Número</th>
+          <th scope="col">Complemento</th>
+          <th scope="col">Ação
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
 
                 // Define as credenciais de conexão ao banco de dados
                 $dbname = "postgres";
@@ -85,7 +89,7 @@ include 'config.php'; ?>
                         </a>
                         </button>
 
-                        <button class="btn2"><a href="delete.php?deleteid='.$id.'" title="Deletar"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#1a89c9" class="bi bi-trash" viewBox="0 0 16 16">
+                        <a href="delete.php?deleteid='.$id.'" onclick="return confirmDelete();" title="Deletar"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#1a89c9" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                         </svg>
