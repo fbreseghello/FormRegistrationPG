0b25949f4f733daeca1353ba4c14b3c4ad08fc30
Felipe B <99092931+fbreseghello@users.noreply.github.com>
2023-03-12 00:56:49 -0300

    Update index.php
    Alterado acesso com banco para railway

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Conexão com postgreSQL 
-$conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=root");
+$conn = pg_connect("host=containers-us-west-167.railway.app port=7530 dbname=railway user=postgres password=Icu8O1v9L70o4XU2SCl4");
 
 // Obter os dados do formulário
 $nome = isset($_POST['nome']) ? pg_escape_string($conn, $_POST['nome']) : "";
